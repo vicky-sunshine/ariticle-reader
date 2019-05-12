4f78a5f157903f1c640b038634bdd9b26483c09c
vicky-sunshine <lch82327@gmail.com>
2019-05-12 22:02:32 +0800

    hn: make hn package to meet article interface

--- hn/hn.go
+++ hn/hn.go
@@ -4,49 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
+	"strconv"
 )
 
-var apiBase = "https://hacker-news.firebaseio.com/v0"
-
-func ItemFormat(item Item) string {
-	u, _ := url.Parse(item.URL)
-	return fmt.Sprintf("%8d  %v (%v)\n          %d points by %v %v | %v comments",
-		item.ID, item.Title, u.Hostname(),
-		item.Score, item.By, DurationFormat(item.Time), item.Descendants)
-}
-
-func TopItems() ([]int, error) {
-	resp, err := http.Get(fmt.Sprintf("%s/topstories.json", apiBase))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	var ids []int
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&ids)
-	if err != nil {
-		return nil, err
-	}
-	return ids, nil
-}
-
-func GetItem(id int) (Item, error) {
-	var item Item
-	resp, err := http.Get(fmt.Sprintf("%s/item/%d.json", apiBase, id))
-	if err != nil {
-		return item, err
-	}
-	defer resp.Body.Close()
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&item)
-	if err != nil {
-		return item, err
-	}
-	return item, nil
-}
-
-type Item struct {
+type HNArticle struct {
 	By          string `json:"by"`
 	Descendants int    `json:"descendants"`
 	ID          int    `json:"id"`
@@ -54,9 +15,73 @@ type Item struct {
 	Score       int    `json:"score"`
 	Time        int64  `json:"time"`
 	Title       string `json:"title"`
-	Type        string `json:"type"`
 
 	// Only one of these should exist
 	Text string `json:"text"`
 	URL  string `json:"url"`
 }
+
+func (hna HNArticle) GetID() string{
+	return strconv.Itoa(hna.ID)
+}
+func (hna HNArticle) GetTitle() string{
+	return hna.Title
+}
+func (hna HNArticle) GetAuthor() string{
+	return hna.By
+}
+func (hna HNArticle) GetTimestamp() int64{
+	return hna.Time
+}
+
+type HackerNewsReader struct {
+	apiBase string
+}
+
+func NewHackerNewsReader() *HackerNewsReader {
+	hnr := &HackerNewsReader{}
+	hnr.apiBase = "https://hacker-news.firebaseio.com/v0"
+	return hnr
+}
+
+func (hnr *HackerNewsReader) GetArticle(id string) (HNArticle, error) {
+	var ar HNArticle
+	resp, err := http.Get(fmt.Sprintf("%s/item/%s.json", hnr.apiBase, id))
+	if err != nil {
+		return ar, err
+	}
+	defer resp.Body.Close()
+
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&ar)
+	if err != nil {
+		return ar, err
+	}
+	return ar, nil
+}
+
+func (hnr *HackerNewsReader) TopArticles(number int) ([]HNArticle, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/topstories.json", hnr.apiBase))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var ids []int
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&ids)
+	if err != nil {
+		return nil, err
+	}
+
+	var ars []HNArticle
+	for _, v := range ids[:number] {
+		ar, err := hnr.GetArticle(strconv.Itoa(v))
+		if err != nil {
+			return ars, err
+		}
+		ars = append(ars, ar)
+	}
+	return ars, nil
+}
+
