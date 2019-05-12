ac29b48ddbdc28241bf1d7c31897d80f2e87b8d1
vicky-sunshine <lch82327@gmail.com>
2019-05-13 01:54:14 +0800

    use cobra to config cli interface

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ require (
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
 	github.com/mmcdole/gofeed v1.0.0-beta2
 	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
 	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5 // indirect
 	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -4,13 +4,44 @@ import (
 	"fmt"
 	"hn-reader/article"
 	"hn-reader/hackernews"
+	"hn-reader/reddit"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	hnr := hackernews.NewHackerNewsReader()
-	list, _ := hnr.TopArticles(10)
-	for _, v := range list {
-		ar, _ := hnr.GetArticle(v)
-		fmt.Println(article.Summerized(ar))
+	var showNum int
+
+	var rootCmd = &cobra.Command{Use: "hn-reader"}
+
+	var cmdHackerNews = &cobra.Command{
+		Use:   "hkns",
+		Short: "Read hacker news",
+		Run: func(cmd *cobra.Command, args []string) {
+			hnr := hackernews.NewHackerNewsReader()
+			list, _ := hnr.TopArticles(showNum)
+			for _, v := range list {
+				ar, _ := hnr.GetArticle(v)
+				fmt.Println(article.Summerized(ar))
+			}
+		},
 	}
+
+	var cmdRedditGolang = &cobra.Command{
+		Use:   "rdgl",
+		Short: "Read reddit /r/golang",
+		Run: func(cmd *cobra.Command, args []string) {
+			rdr := reddit.NewRedditReader("https://www.reddit.com/r/golang")
+			list, _ := rdr.TopArticles(showNum)
+			for _, v := range list {
+				ar, _ := rdr.GetArticle(v)
+				fmt.Println(article.Summerized(ar))
+			}
+		},
+	}
+
+	rootCmd.AddCommand(cmdHackerNews, cmdRedditGolang)
+	rootCmd.PersistentFlags().IntVar(&showNum, "number", 10, "Use Viper for configuration")
+	rootCmd.Execute()
+
 }
